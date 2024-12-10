501e1b4cd42c14464f679001b2652fb55669c9f5
erikabelissima <erikaalvess2006@gmail.com>
2024-12-09 21:10:18 -0300

    primeiro comit

---
+++ atividade01.js
@@ -0,0 +1,8 @@
+//Declare uma variavel para armazenar o nome de um aluno, sua idade, e 
+//sua nota final, Exiba valores no console. 
+
+let nome = "erika carvalho"
+let idade = 18
+let notaFinal =  9.5
+
+console.log("nome: ", nome, "Idade", idade, "Nota final: ", notaFinal)
